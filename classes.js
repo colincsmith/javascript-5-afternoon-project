8f59ab47d35b88b91d78747f391987c06ca419cb
colin smith <colincraigsmith@colincrcbookpro.lan>
2020-10-19 16:56:27 -0600

    black diamond finished as well

--- classes.js
+++ classes.js
@@ -150,4 +150,26 @@ class ProgressiveManager extends Manager {
 
 //Code Here
 
-
+class Machine{
+  constructor(){
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+  makeWidgets(num){
+    this.widgets_made_count += num
+    this.wear_and_tear_count = 0
+    for(let i = 0; i < this.widgets_made_count; i += 50){
+      this.wear_and_tear_count ++;
+    }
+  }
+  fixMachine(){
+    this.needs_reboot = true
+  }
+  reboot(){
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+}
